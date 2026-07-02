30deb765a384c1111e7f8376e31d151079285cc4
mohamedaffif <twaha2030@gmail.com>
2026-07-02 17:39:51 +0300

    setting up prisma and supabase

---
+++ utils/supabase/client.ts
@@ -0,0 +1,9 @@
+// for client side usage, we can use the createBrowserClient function from @supabase/ssr package
+import { createBrowserClient } from "@supabase/ssr";
+
+export function createClient() {
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+}
