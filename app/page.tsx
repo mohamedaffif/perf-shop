5dd72c1be429ebbd822ebb84f251daffa5c9b3d8
mohamedaffif <twaha2030@gmail.com>
2026-07-04 21:24:40 +0300

    added animation.css

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
+    <div className="flex min-h-screen items-center justify-center">
       <h1 className="text-5xl font-bold">
-        Perf Shop
+        <div className="flex justify-between rounded-lg bg-black p-4 text-white">hello</div>
       </h1>
       <Button>click me </Button>
     </div>
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,17 @@
-import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import eslintConfigPrettier from "eslint-config-prettier";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+
+  // Disable formatting rules that conflict with Prettier
+  eslintConfigPrettier,
+
   // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
 
 export default eslintConfig;
