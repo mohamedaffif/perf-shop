bd34d67ba4fa00ebdcc559a19512c69d2cace035
mohamedaffif <twaha2030@gmail.com>
2026-09-22 16:46:43 +0300

    formated the sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,7 +5,6 @@ import { listProducts } from "@/domain/product";
 
 export const dynamic = "force-dynamic";
 
-
 const siteUrl = process.env.NEXT_PUBLIC_APP_URL ?? "http://localhost:3000";
 
 async function listPublishedProductIds(): Promise<string[]> {
